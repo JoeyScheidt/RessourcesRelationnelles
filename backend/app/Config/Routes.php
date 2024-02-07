20f6672a1be4b9a870e7dd191b9f31e7fe6a595d
qBor <kevin.fosse057@gmail.com>
2024-02-07 14:24:06 +0100

    Exemple CRÉATION api

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+// https://codeigniter.com/user_guide/incoming/restful.html
+$routes->get('api/helloworld', 'HelloWorld::index');
