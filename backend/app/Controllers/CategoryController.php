e99adcf1d40343a82cef398d8f88e1245c8f7d49
JoeyScheidt <scheidt.joey@gmail.com>
2024-02-07 13:41:04 +0100

    création des modèles et contrôleurs

---
+++ backend/app/Controllers/CategoryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class CategoryController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
