1fc25340b0db53815ce1f7538c5a7b97d43b2f25
Thomas <thomas.doppler00@gmail.com>
2024-02-07 10:39:49 +0100

    debut interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
-import Header from './Header/Header';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Registration from './components/Registration/Registration';
+import Login from './components/Login/Login';
+import Help from "./components/Help/Help";
+import Resources from "./components/Resources/Resources";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header />
+      <Header />
 
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/help" element={<Help />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
